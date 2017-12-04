907263b513ff4ab9b58e292009d9c4da28f68ba6
Camus-Ferland Didier <1458214@cegepmontpetit.ca>
2017-12-04 15:32:19 -0500

    Fixed the code

--- VoyageAngularWebsite/src/app/app.module.ts
+++ VoyageAngularWebsite/src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { FormComponent }  from './form.component';
-import {VoyageComponent} from "./voyage.component";
-import {ActiviteComponent} from "./activite.component";
-import {JourComponent} from "./jour.component";
-import {TransportComponent} from "./transport.component";
-import {ActiviteService} from "./activite.service";
-import {JourService} from "./jour.service";
-import {TransportService} from "./transport.service";
-import {VoyageService} from "./voyage.service";
-import {HttpModule} from "@angular/http";
-import {UserService} from "./user.service";
-import {UserComponent} from "./user.component";
+import { VoyageComponent } from "./voyage.component";
+import { ActiviteComponent } from "./activite.component";
+import { JourComponent } from "./jour.component";
+import { TransportComponent } from "./transport.component";
+import { ActiviteService } from "./activite.service";
+import { JourService } from "./jour.service";
+import { TransportService } from "./transport.service";
+import { VoyageService } from "./voyage.service";
+import { HttpModule } from "@angular/http";
+import { UserService } from "./user.service";
+import { UserComponent } from "./user.component";
+import { RouterModule } from "@angular/router";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
     imports:      [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: '/home', pathMatch: 'full' },
+            { path: 'home', component: VoyageComponent }
+        ])
     ],
     declarations: [
         FormComponent,
