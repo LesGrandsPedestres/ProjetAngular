a790bc229f3300cde0ffbd502770bbcdaf32683b
Lemire Pierre-Luc <1045379@cegepmontpetit.ca>
2017-12-04 16:05:43 -0500

    Added Destination class and Destination to Transport

--- WebApiVoyage/WebApiVoyage/Models/Destination.cs
+++ WebApiVoyage/WebApiVoyage/Models/Destination.cs
@@ -7,5 +7,9 @@ namespace WebApiVoyage.Models
 {
     public class Destination
     {
+        public double LongitudeDepart { get; set; }
+        public double LatitudeDepart { get; set; }
+        public double LongitudeArrivee { get; set; }
+        public double LatitudeArrivee { get; set; }
     }
 }
