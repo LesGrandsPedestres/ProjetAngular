eb994f7a75741b56252b5cf313af5936510e997f
Zachary Leclerc <34038289+TiltingIsFun@users.noreply.github.com>
2017-12-04 14:55:08 -0500

    push My Crap Js + CSS

--- VoyageAngularWebsite/src/app/voyage.component.ts
+++ VoyageAngularWebsite/src/app/voyage.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'voyage',
-  templateUrl: './voyage.component.html',
+  templateUrl: './voyage.component.html'
 })
 export class VoyageComponent {
+  //a include : javascriptStyle.js + styles.css
 
 }
