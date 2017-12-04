9f824e850f3179a2d146aadd79989b69761b125f
VLuss <34038356+VLuss@users.noreply.github.com>
2017-12-04 13:29:47 -0500

    debut

--- VoyageAngularWebsite/src/app/form.component.ts
+++ VoyageAngularWebsite/src/app/form.component.ts
@@ -2,24 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-form',
-  template: `    
-  <div class="container" style="margin-top: 25px">
-    <div class="row">
-      <div class="col-md-3">
-        <input class="form-control" [(ngModel)]="artist.name" placeholder="Nouvel artiste">
-      </div>
-      <div class="col-md-9">
-        {{artist.name}}
-      </div>
-    </div>
-  </div>
-  `,
+  templateUrl: './form.component.html',
 })
 export class FormComponent {
-  artist: Artist = {id: 1, name: ''};
-}
-
-export class Artist {
-  id: number;
-  name: string;
+  
 }
