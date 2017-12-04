74670edcf560bf437e7d1b55febee740f4abb4dd
Lemire Pierre-Luc <1045379@cegepmontpetit.ca>
2017-12-04 14:10:44 -0500

    Base files added for Angular project

--- VoyageAngularWebsite/src/app/app.module.ts
+++ VoyageAngularWebsite/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { FormComponent }  from './form.component';
+import {VoyageComponent} from "./voyage.component";
+import {ActiviteComponent} from "./activite.component";
+import {JourComponent} from "./jour.component";
+import {TransportComponent} from "./transport.component";
+import {ActiviteService} from "./activite.service";
+import {JourService} from "./jour.service";
+import {TransportService} from "./transport.service";
+import {VoyageService} from "./voyage.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ FormComponent ],
+  imports:      [ BrowserModule, FormsModule],
+  declarations: [ FormComponent,
+                  ActiviteComponent,
+                  JourComponent,
+                  TransportComponent,
+                  VoyageComponent ],
+  providers:    [ActiviteService,
+                 JourService,
+                 TransportService,
+                 VoyageService],
   bootstrap:    [ FormComponent ]
 })
 export class AppModule { }
