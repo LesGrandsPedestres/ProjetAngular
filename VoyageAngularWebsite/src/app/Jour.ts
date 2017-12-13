2879215aebdd4e104a5b0379ea71b90e1fd10ff5
Zachary Leclerc <34038289+TiltingIsFun@users.noreply.github.com>
2017-12-13 14:05:58 -0500

    fix de etienne

--- VoyageAngularWebsite/src/app/Jour.ts
+++ VoyageAngularWebsite/src/app/Jour.ts
@@ -6,7 +6,7 @@ export class Jour{
 }
 
 export const JOURS:Jour[] = [
-    {JourId:1,NumeroJour:1,BudgetJour:500,Voyage:1},
-    {JourId:2,NumeroJour:2,BudgetJour:600,Voyage:1},
-    {JourId:3,NumeroJour:3,BudgetJour:400,Voyage:1},
+    {JourId:1,NumeroJour:1,BudgetJour:500,VoyageId:1},
+    {JourId:2,NumeroJour:2,BudgetJour:600,VoyageId:1},
+    {JourId:3,NumeroJour:3,BudgetJour:400,VoyageId:1},
 ]
