91e11b65aab390b72c62860f56d5dfd9cddaa705
Zachary Leclerc <34038289+TiltingIsFun@users.noreply.github.com>
2017-12-13 11:59:24 -0500

    Controlleur Jours

---
+++ WebApiVoyage/WebApiVoyage/Controllers/JoursController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiVoyage.Models;
+
+namespace WebApiVoyage.Controllers
+{
+    public class JoursController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Jours
+        [Route("api/Jours/GetJoursByVoyageId/{VoyageId}")]
+        public List<JourDTO> GetJoursByVoyageId(int VoyageId)
+        {
+            return Jour.toDTOList(db.Jours.Where(x=>x.VoyageId == VoyageId).ToList());
+        }
+
+        // GET: api/Jours/5
+        [ResponseType(typeof(JourDTO))]
+        [Route("api/Jours/GetJourById/{JourId}")]
+        public JourDTO GetJourById(int JourId)
+        {
+            Jour jour = db.Jours.Find(JourId);
+            if (jour == null)
+            {
+                return null;
+            }
+
+            return jour.toDTO();
+        }
+
+        // PUT: api/Jours/5
+        [ResponseType(typeof(void))]
+        [Route("api/Jours/ModifyJour/{id}")]
+        public IHttpActionResult PutJour(int id, Jour jour)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != jour.JourId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(jour).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JourExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Jours
+        [ResponseType(typeof(Jour))]
+        [Route("api/Jours/AddJour")]
+        public IHttpActionResult PostJour(Jour jour)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            db.Jours.Add(jour);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = jour.JourId }, jour);
+        }
+
+        // DELETE: api/Jours/5
+        [ResponseType(typeof(JourDTO))]
+        [Route("api/Jours/RemoveJour/{id}")]
+        public IHttpActionResult DeleteJour(int id)
+        {
+            Jour jour = db.Jours.Find(id);
+            if (jour == null)
+            {
+                return NotFound();
+            }
+
+            db.Jours.Remove(jour);
+            db.SaveChanges();
+
+            return Ok(jour.toDTO());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool JourExists(int id)
+        {
+            return db.Jours.Count(e => e.JourId == id) > 0;
+        }
+    }
+}
