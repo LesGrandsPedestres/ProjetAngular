90f4436d7c9bf359f2beafc310a607d0874baa7d
Lussier Etienne <1543677@cegepmontpetit.ca>
2017-12-13 13:41:30 -0500

    Module Create voyage component plus path

--- VoyageAngularWebsite/src/app/Jour.ts
+++ VoyageAngularWebsite/src/app/Jour.ts
@@ -4,3 +4,9 @@ export class Jour{
     BudgetJour:number;
     VoyageId:number;
 }
+
+export const JOURS:Jour[] = [
+    {JourId:1,NumeroJour:1,BudgetJour:500,Voyage:1},
+    {JourId:2,NumeroJour:2,BudgetJour:600,Voyage:1},
+    {JourId:3,NumeroJour:3,BudgetJour:400,Voyage:1},
+]
