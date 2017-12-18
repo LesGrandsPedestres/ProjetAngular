3c0ab49652a39c671ba69363832c4558d811e855
Lussier Etienne <1543677@cegepmontpetit.ca>
2017-12-18 09:42:04 -0500

    Modification du component Jour

--- VoyageAngularWebsite/src/app/activite.component.ts
+++ VoyageAngularWebsite/src/app/activite.component.ts
@@ -12,13 +12,15 @@ import {Jour, JOURS} from "./Jour";
 export class ActiviteComponent  implements  OnInit{
     Jours:Jour[] = JOURS
     @Input() voyageId: string; // Pour prendre toute les jours de se voyages
-    JoursIdSelected:string; //Pour prendre toute les activitées de cette journée afin de pouvoir mettre ne place un horaire
+    @Input() JoursId:string;
+    //JoursIdSelected:string; //Pour prendre toute les activitées de cette journée afin de pouvoir mettre ne place un horaire
 
     constructor (private serivce:ActiviteService, private route:ActivatedRoute){ }
 
     ngOnInit():void{
         this.route.paramMap.subscribe(params => {
-            this.voyageId = params.get('id');
+            this.voyageId = params.get('voyageId');
+            this.JoursId = params.get('jourId');
             //this.serivce.getActivites(this.voyageId);
         })
     }
--- VoyageAngularWebsite/src/app/jour.component.ts
+++ VoyageAngularWebsite/src/app/jour.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {JOURS, Jour} from "./Jour";
+import {ActivatedRoute} from "@angular/router";
+import {ActiviteService} from "./activite.service";
 
 @Component({
   selector: 'jour',
   templateUrl: './jour.component.html',
 })
-export class JourComponent {
+export class JourComponent implements  OnInit{
+      listJours:Jour[] = JOURS;
+      voyageId:string;
+
+  constructor (private serivce:ActiviteService, private route:ActivatedRoute){ }
+
+  ngOnInit():void{
+    this.route.paramMap.subscribe(params => {
+      this.voyageId = params.get('voyageId').toString();
+      //this.serivce.getActivites(this.voyageId);
+    })
+  }
 
 }
